8be76a0904e8528f1077f429e4780741096f9836
muhtarudinsiregar <6048482+muhtarudinsiregar@users.noreply.github.com>
2018-11-29 14:40:36 +0700

    Remove unused code

--- components/Button.js
+++
@@ -1,8 +0,0 @@
-const Button = (props) => (
-    <button type="button" onClick={this.toUpperCase}>UPPERCASE</button>
-    <button type="submit" onClick={this.handleSubmit}>Lowercase</button>
-    <button type="submit" onClick={this.handleSubmit}>camelCase</button>
-    <button type="submit" onClick={this.handleSubmit}>snake_case</button>
-)
-
-export default Button
