6030a1892adb5840e112e5ce72bac04f63fd94ad
muhtarudinsiregar <6048482+muhtarudinsiregar@users.noreply.github.com>
2018-11-29 15:52:37 +0700

    Deploy gh-pages

---
+++ _next/static/SU3xsaATC7eDkA6uI6jWN/pages/about.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{205:function(e,n,t){__NEXT_REGISTER_PAGE("/about",function(){return e.exports=t(206),{page:e.exports.default}})},206:function(e,n,t){"use strict";t.r(n);var u=t(0),a=t.n(u);n.default=function(){return a.a.createElement("div",null,a.a.createElement("p",null,"this is  about page"))}}},[[205,1,0]]]);
